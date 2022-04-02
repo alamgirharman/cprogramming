7704a24eea7535d855a893461c8c674f8f0edc90
sumon <sumonahmed27@gmail.com>
2022-04-02 12:42:56 +0600

    completed chap 2

--- cpb/two15.c
+++ cpb/two15.c
@@ -7,11 +7,23 @@ int main() {
     printf("Please enter a number: ");
     scanf("%d", &num1);
     printf("Please enter another number: ");
-    scanf("%d", num2);
+    scanf("%d", &num2);
 
     value = num1 + num2;
     sign = '+';
-    printf("%d %c %d = %d", num1, sign, num2, value);
+    printf("%d %c %d = %d\n", num1, sign, num2, value);
+
+    value = num1 - num2;
+    sign = '-';
+    printf("%d %c %d = %d\n", num1, sign, num2, value);
+
+    value = num1 * num2;
+    sign = '*';
+    printf("%d %c %d = %d\n", num1, sign, num2, value);
+
+    value = num1 / num2;
+    sign = '/';
+    printf("%d %c %d = %d\n", num1, sign, num2, value);
     
     return 0;
 }
---
+++ cpb/two16.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // test program - comment 1
+    printf("Hello ");
+
+    /* We have printed Hello, now we shal print World.
+    Note that this is a muti-line comment */
+    printf("World\n"); // printed world
+
+    return 0;
+}
