dc7fa3c92a951798670eec00bfcb08547ba22967
sumon <sumonahmed27@gmail.com>
2022-04-13 17:06:32 +0600

    ch4 run

---
+++ cpb/four3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// caution!!! infinite loop !!!
+
+int main() {
+    int n = 1;
+    while(n <= 10) {
+        printf("%d\n", n);
+    }
+    n++;
+    return 0;
+}
+
+// caution!!! infinite loop !!!
